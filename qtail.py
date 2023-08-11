47478acf9a92a7dd0b2d8cd38ef0ebbba2de4918
XiaozhiSans <xiaozhisans@qq.com>
2023-08-11 17:33:28 +0800

    qtail for webpage

--- qtail.py
+++ qtail.py
@@ -1,5 +1,4 @@
-while 0 == 0:
-    name = str(input("请输入你想要的昵称:\n(如：XiaozhiSans)"));
-    tail = str(input("请输入你想要的尾巴:\n(如:“喵~”)"));
-    print("生成完毕，请复制以下一整行内容并更改你的qq昵称");
-    print(name + "⁧" + tail + "⁦")
+name = str(input("请输入你想要的昵称:\n(如：XiaozhiSans)"));
+tail = str(input("请输入你想要的尾巴:\n(如:“喵~”)"));
+print("生成完毕，请复制以下一整行内容并更改你的qq昵称");
+print(name + "⁧" + tail + "⁦")
