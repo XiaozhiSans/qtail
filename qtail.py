d1509f650557ad06aeedb67e21358231e1451992
XiaozhiSans <xiaozhisans@qq.com>
2023-08-12 11:09:31 +0800

    Fixed a serious bug

--- qtail.py
+++ qtail.py
@@ -1,4 +1,4 @@
 name = str(input("请输入你想要的昵称:\n(如：XiaozhiSans)"));
-tail = str(input("请输入你想要的尾巴:\n(如:“喵~”)"));
+tail = str(input("请输入你想要的尾巴:\n(需要反写，如:“喵~”就输入“~喵”)"));
 print("生成完毕，请复制以下一整行内容并更改你的qq昵称");
 print(name + "⁧" + tail + "⁦")
